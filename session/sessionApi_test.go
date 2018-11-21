10d622208b7644011f8cc310f8c48df37ee628f4
Roy <roy@RoydeMacBook-Pro.local>
2018-11-21 14:02:07 +0800

    bug fix

--- session/sessionApi_test.go
+++ session/sessionApi_test.go
@@ -8,7 +8,7 @@ func TestSession(t *testing.T){
 	sessionContext := memorySession.NewSessionManager()
 	session:=sessionContext.GetSession("roy",true)
 	session.AddParam("address","shenzhen")
-	assert.Equal(t,false,session.IsExpire,"session init is not expire")
+	assert.Equal(t,false,session.IsExpire(),"session init is not expire")
 	session.ExpireImmediately()
 	assert.True(t,session.IsExpire(),"session expire faild")
 }
