13adff0e7822218c6a25fa0d6295a866111bbc78
袁瑞 <Roy@yuanruideMacBook-Pro.local>
2018-11-21 12:00:20 +0800

    add session support

---
+++ session/api.go
@@ -0,0 +1,16 @@
+package session
+
+type Session interface {
+	//add param into a session
+	AddParam(key string, val interface{})
+	//remove param from a session,and return the param
+	RemoveParam(key string) interface{}
+	//expore the session
+	ExpireImmediately()
+	//return is the session expired
+	IsExpire() bool
+}
+
+type SessionManager interface {
+	GetSession(isCreateOnNil bool) Session
+}
